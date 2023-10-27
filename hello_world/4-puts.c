e5581c8f5959bb410bc9b2df06aa549fcf21bf29
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-10-27 13:25:50 -0700

    Se agrega Script N°4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - C program: Prints "Programming is like building a multilingual puzzle
+ *
+ * Return - Always 0.
+ */
+int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
-
-
