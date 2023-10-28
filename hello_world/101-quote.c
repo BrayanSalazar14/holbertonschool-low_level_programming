c8d8222f553f93ec7eb4379c9b522f1b94ece2b7
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-10-27 23:49:44 -0700

    Test Advanced 1

---
+++ hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/*
+ * File: 101-quote.c
+ * Author: Brayan Steven Salazar
+ */
+
+#include<stdio.h>
+/**
+ * main - C program that prints already stored text
+ *
+ * Return: 1.
+ */
+int main(void)
+{
+        putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+        return (1);
+
+}
+
