7f4f085310d4faddde9c479e6a761ab8f65961ce
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-10-28 01:10:48 -0700

    Final Test

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -12,8 +12,7 @@
  */
 int main(void)
 {
-	write(stderr, "and that piece of art is useful\"
-		- Dora Korpar, 2015-10-19\n", 57);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 
 }
