851c8b2c6838fdac053eca071f8efcf7eccf3ad4
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-10-30 16:14:41 -0700

    Changes Code

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -13,12 +13,13 @@
 int main(void)
 {
 	int n = 0;
-
+	/* Loop to print the digits from 0 to 9 */
+	/* Every time n is less than 10 it will continue to iterate */
 	while (n < 10)
 	{
 		printf("%d", n);
 		n++;
 	}
-	printf("\n");
+	printf("\n"); /* Line break */
 	return (0);
 }
--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -12,13 +12,17 @@
  */
 int main(void)
 {
-	int n = 48;
+	int n = 48; /* It is declared with the value 48 of the ASCII code */
+		      /* which is equivalent to 0 */
 
+
+	/* Loop to print the numbers from 0 to 9 */
+	/* n = 48 ASCII code and 9 = 57 ASCII code */
 	while (n <= 57)
 	{
 		putchar(n);
 		n++;
 	}
-	putchar('\n');
+	putchar('\n'); /* Line break */
 	return (0);
 }
