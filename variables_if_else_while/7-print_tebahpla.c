49b30cea153077f4f49e8635ffafdec5f0b7f0d7
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-10-30 16:40:14 -0700

    Task N°8 is added

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+/*
+ *File: 2-print_alphabet.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include<stdio.h>
+
+/**
+ * main - Program that prints the reverse alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letters; /* Variable is declared to represent a letter */
+
+	/* Prints lowercase letters from 'z' to 'a' */
+	for (letters = 'z'; letters >= 'a' ; letters--)
+	{
+		putchar(letters);
+	}
+
+	putchar('\n'); /* Line break */
+	return (0);
+}
