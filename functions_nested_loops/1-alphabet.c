1406177779a9cc5d75580e5c0d7859de7d8571b5
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-10-31 13:56:30 -0700

    Add Task 1,2,3

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+/*
+ *File: 1-alphabet.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *print_alphabet - Function that prints the alphabet, in lowercase
+ *
+ *Return: Always void
+ */
+void print_alphabet(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters < 'z'; letters++)
+	{
+		_putchar(letters);
+	}
+	_putchar('\n');
+	return (void);
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+/*
+ *File: 2-print_alphabet_x10.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Function that prints 10 times the alphabet
+ *in lowercase, followed by a new line.
+ *
+ *Return: Always void.
+ */
+void print_alphabet_x10(void)
+{
+	int letters;
+	int cant;
+
+	for (cant = 0; cant < 10; cant++)
+	{
+		for (letters = 'a'; letters < 'z'; letters++)
+		{
+			_putchar(letters);
+		}
+	_putchar('\n');
+	}
+	return (void);
+}
