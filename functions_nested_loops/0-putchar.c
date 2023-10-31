0dd1ba788ae8e19aef1090e402ee2c0991a9f98b
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-10-31 09:08:08 -0700

    Changes and add

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
  *Author: Brayan Steven Salazar
  */
 
-#include<stdio.h>
+#include "main.h"
 
 /**
  *main - Program that prints _putchar
@@ -12,6 +12,14 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
 	return (0);
 }
