dce1aaf4f88f885c8d3d8042096c6c359e8b0d10
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-01 11:06:25 -0700

    Task changes

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,12 @@ int print_last_digit(int n)
 	{
 		lstd = (n * -1) % 10;
 		_putchar(lstd + '0');
+		return (lstd);
 	}
 	else if (n > 0 || n == 0)
 	{
 		lstd = n % 10;
 		_putchar(lstd + '0');
+		return (lstd);
 	}
-	return (lstd);
 }
