58397e0d462b2e62a14b7f1fb54ccfd9b460b2e2
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-01 13:44:35 -0700

    Task N°8 changes

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -4,8 +4,8 @@
  */
 
 #include "main.h"
-/*
- *jack_bauer - Function that prints every minute of the day of 
+/**
+ *jack_bauer - Function that prints every minute of the day of
  *Jack Bauer, starting from 00:00 to 23:59
  *
  *Return: void
@@ -15,9 +15,9 @@ void jack_bauer(void)
 	int hours;
 	int minutes;
 
-	for(hours = 0; hours < 24; hours++)
+	for (hours = 0; hours < 24; hours++)
 	{
-		for(minutes = 0; minutes < 60; minutes++)
+		for (minutes = 0; minutes < 60; minutes++)
 		{
 			_putchar((hours / 10) + '0');
 			_putchar((hours % 10) + '0');
