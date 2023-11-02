f6fb6d9347b235c7d775726f8e04e01fb7ae3270
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-01 21:37:36 -0700

    Task N°11 added

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+/*
+ *File: 11-print_to_98.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ *print_to_98 - Function that prints all natural numbers from n to 98
+ *followed by a new line
+ *@n: Declaration of the integer variable
+ *Return: void
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	}
+}
