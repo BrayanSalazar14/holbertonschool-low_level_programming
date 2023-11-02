ed0be0de9fa291dd6ddc5d84b415a892f2dd806e
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-01 20:45:23 -0700

    Task N°10 added

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+ *File: 10-add.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *add - Function that adds two integers and returns the result
+ *@n1: Integer variable declaration
+ *@n2: Integer variable declaration
+ *Return: Result of the sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	return ((n1 + n2));
+}
