d2a28a89457f9df141d8cf31b0fc05cda9bd714d
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-03 16:40:40 -0700

    Task N°9 added

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+/*
+ *File: 9-fizz_buzz.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include <stdio.h>
+
+/**
+ *main - Print numbers 1-100, Fizz for 3, Buzz for 5, FizzBuzz for both.
+ *
+ *Return: void
+ */
+int main (void)
+{
+	int i;
+
+	for(i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+
+		else if (i % 5 == 0)
+				printf("Buzz ");
+
+		else if (i % 3 == 0 && i % 5 == 0)
+					printf("Fizz Buzz ");
+
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
