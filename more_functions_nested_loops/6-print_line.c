e7250d1952fb4c47c543cc657b81e83bc51e1c56
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-03 11:29:27 -0700

    Task N°6 added

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+/*
+ *File: 6-print_line.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *print_line - Function that draws a straight line in the terminal
+ *@n: Integer variable declaration
+ *Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
