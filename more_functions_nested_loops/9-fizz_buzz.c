4316b5c31c7f56d6373470c165807aca2696cbf7
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-03 16:50:09 -0700

    Task Changes

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,20 +10,20 @@
  *
  *Return: void
  */
-int main (void)
+int main(void)
 {
 	int i;
 
-	for(i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("Fizz Buzz ");
+
+		else if (i % 3 == 0)
 			printf("Fizz ");
 
 		else if (i % 5 == 0)
-				printf("Buzz ");
-
-		else if (i % 3 == 0 && i % 5 == 0)
-					printf("Fizz Buzz ");
+			printf("Buzz ");
 
 		else
 			printf("%d ", i);
