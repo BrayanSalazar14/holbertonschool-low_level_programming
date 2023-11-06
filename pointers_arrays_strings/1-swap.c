0ba7d410d97b74f5b8161ad020f32fcef96529f4
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-06 07:49:15 -0800

    Task N°1 added

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+/*
+ *File: 1-swap.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *swap_int - Function that swaps the values of two integers
+ *@a: Pointer type variable
+ *@b: Pointer type variable
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
