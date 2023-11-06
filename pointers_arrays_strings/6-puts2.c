7c0cc3dd4ea5b514804427723132739428f4f96a
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-06 14:08:36 -0800

    Task N°6 added

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+/*
+ *File: 6-puts2.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *puts2 - Function that prints every other character of a string
+ *starting with the first character
+ *@str: Declaration of variable type char
+ *Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
