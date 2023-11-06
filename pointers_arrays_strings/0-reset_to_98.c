f4e4c8f8c6f0874d55c6bb758f1f5b44ce97bb37
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-06 07:34:33 -0800

    Add new Project and Task N°0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ *File: 0-reset_to_98.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *reset_to_98 - Function that takes a pointer to an int as parameter
+ *and updates the value it points to to 98
+ *@n: function that takes a pointer to an int as parameter
+ *and updates the value it points to to 98
+ *Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
