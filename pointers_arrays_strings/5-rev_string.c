e8d6ac3c2958548a5efd6a6b3382ed1c0455ce03
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-06 12:00:47 -0800

    Task N°5 added

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+/*
+ *File: 5-rev_string.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *rev_string - Function that reverses a string.
+ *@s: Declaration of variable type char
+ *Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j, revS;
+
+	for (int i = 0; s[i] != '\0'; i++)
+		;
+
+	for (j = 0; j < i / 2; j++)
+	{
+		revS = s[j];
+		s[j] = s[i - 1 - j];
+		s[i - 1 - j] = revS;
+	}
+}
