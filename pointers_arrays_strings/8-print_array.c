6521fd76c65a3c52464dc3a4fd11f326aab873bb
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-07 12:15:51 -0800

    Task N°8 added

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+/*
+ *File: 8-print_array.c
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *print_array - Function that prints n elements of an array of integers
+ *@a: variable integer pointer array
+ *@n: Integer quantity variable
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
