84271e603280db582aa8c2bdb97d10f31d5ed0e7
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-08 10:57:18 -0800

    Task N°0 added

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
 int _putchar(char c);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
@@ -13,5 +14,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+char *_strcat(char *dest, char *src);
 
 #endif
