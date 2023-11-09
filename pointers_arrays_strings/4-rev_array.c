b849887ab377351af0dd7514df859b593d4a1664
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-09 13:51:46 -0800

    Changes

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -15,10 +15,10 @@ void reverse_array(int *a, int n)
 	int i = 0;
 	int rev;
 
-	for (i = 0; i < n / 2; i++)
+	for (i = n - 1; i >= n / 2; i--)
 	{
 		rev = a[i];
-		a[i] = a[i - 1 - n];
-		a[i - 1 - n] = rev;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = rev;
 	}
 }
