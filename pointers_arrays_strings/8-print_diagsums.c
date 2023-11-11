e5b6069e6798c3125c52b3df63fa0db21c0b3da6
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-10 18:40:46 -0800

    Task added

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_diagsums - Function that prints the sum of the two
+ *diagonals of a square matrix of integers.
+ *@a: pointer integer variable declaration
+ *@size Integer variable declaration
+ *Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int diag1 = 0;
+	int diag2 = 0;
+
+	for (int i = 0; i < size; ++i)
+
+	{
+		diag1 += a[i * size + i];
+	}
+
+	for (int i = 0; i < size; ++i)
+
+	{
+		diag2 += a[i * size + (size - 1 - i)];
+	}
+
+	printf("%d, %d\n", diag1, diag2);
+}
