32da6716c2227c068b3159844c5fdb7fc69c42fb
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-10 18:46:08 -0800

    Task added

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@
  *print_diagsums - Function that prints the sum of the two
  *diagonals of a square matrix of integers.
  *@a: pointer integer variable declaration
- *@size Integer variable declaration
+ *@size: Integer variable declaration
  *Return: void
  */
 void print_diagsums(int *a, int size)
