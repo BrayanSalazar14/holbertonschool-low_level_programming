6ba32678f0ab5f1b78c5c9723f2be2bfc3f373cf
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-10 18:42:39 -0800

    Task added

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -16,14 +16,15 @@ void print_diagsums(int *a, int size)
 {
 	int diag1 = 0;
 	int diag2 = 0;
+	int i;
 
-	for (int i = 0; i < size; ++i)
+	for (i = 0; i < size; ++i)
 
 	{
 		diag1 += a[i * size + i];
 	}
 
-	for (int i = 0; i < size; ++i)
+	for (i = 0; i < size; ++i)
 
 	{
 		diag2 += a[i * size + (size - 1 - i)];
