cdda7b01e9a0ca20f82cc9e80c45032c8789bc84
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-14 07:56:20 -0800

    Changes for Task

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,7 +5,7 @@
 #include "main.h"
 
 /**
- *_print_rev_recursion: Function that prints a string in reverse
+ *_print_rev_recursion - Function that prints a string in reverse
  *@s: Char pointer variable declaration
  *Return: void
  */
