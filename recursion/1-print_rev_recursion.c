13b4f948471c3f2bbca9fdcad9be911525d1b4ea
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-14 07:55:05 -0800

    Added Task

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *_print_rev_recursion: Function that prints a string in reverse
+ *@s: Char pointer variable declaration
+ *Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
