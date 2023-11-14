0af17238236d8c8e0db1c949e7e0b6097fb6d313
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-14 08:00:07 -0800

    Task added

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ *_strlen_recursion - Function that returns the length of a string.
+ *@s: Char pointer variable declaration
+ *Return: Chain length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	return 1 + _strlen_recursion(s + 1);
+}
