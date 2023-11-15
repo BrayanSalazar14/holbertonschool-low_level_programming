3c12eb5816c4e8970755eb10e17b3df31c70244d
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-15 11:36:22 -0800

    Task added

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - Program that prints its name, followed by a new line.
+ *@argc: Variable that represents the number of arguments
+ *@argv: Variable pointing to character string
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
