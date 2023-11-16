1f5d6ab9755d91757347eef54a9219dfd6f10f3f
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-16 13:36:42 -0800

    Hot fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,17 +6,24 @@ char *str_concat(char *s1, char *s2)
 {
   char *string;
 
+  if (s1 == NULL)
+  {
+	  s1 = "";
+  }
+
+  if (s2 == NULL)
+  {
+	  s2 = "";
+  }
+
   string = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 
   if (string == NULL)
-  {
-    return (NULL);
-  }
+	  return (NULL);
 
   strcpy(string, s1);
-  
+
   strcat(string, s2);
 
   return string;
-  
 }
