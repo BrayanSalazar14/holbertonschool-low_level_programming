b394eb713f9ed58a40cdbd715183e2643e4f9613
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-17 10:32:40 -0800

    changes for betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -8,7 +8,7 @@
  *free_grid - function that frees a 2 dimensional grid previously
  *created by your alloc_grid function.
  *@grid: The two-dimensional grid to be freed.
- *height: The height of the grid (number of rows).
+ *@height: The height of the grid (number of rows).
  *Return: void
  */
 void free_grid(int **grid, int height)
