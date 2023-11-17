722a69cc8e072f1a71a401d7bad10fe86a84933c
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-17 10:27:46 -0800

    add task

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_grid - function that frees a 2 dimensional grid previously
+ *created by your alloc_grid function.
+ *@grid: The two-dimensional grid to be freed.
+ *height: The height of the grid (number of rows).
+ *Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height)
+		free(grid[i]);
+
+	free(grid);
+}
