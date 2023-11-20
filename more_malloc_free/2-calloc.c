dbd88e1ff046c3c1b0add2de552e28f0580b3dff
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-20 15:01:59 -0800

    hot fix

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+  void *asignMemory;
+  
+  if (nmemb == 0 || size == 0)
+  {
+    return NULL;
+    }
+  
+  asignMemory = malloc(nmemb * size);
+  
+  if (asignMemory == NULL)
+  {
+    return NULL;
+    }    
+  }
