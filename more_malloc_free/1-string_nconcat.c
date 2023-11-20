0dee1df06be8027401218c21268a043f01bbfdd7
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-20 14:42:02 -0800

    move for my variables

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -19,6 +19,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concatString;
+	size_t s1_length, s2_length;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -26,8 +27,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	size_t s1_length = strlen(s1);
-	size_t s2_length = strlen(s2);
+	s1_length = strlen(s1);
+	s2_length = strlen(s2);
 
 	if (n >= s2_length)
 		n = s2_length;
