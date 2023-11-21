81e65a8a8bdfabb095bb9f5007855b31901a06b5
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-21 15:10:33 -0800

    task added

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,9 +8,9 @@ void print_dog(struct dog *d)
 		return;
 	}
 
-	printf("%s\n", (d->name ? d->name : "nail"));
+	printf("%s\n", (d->name ? d->name : "nil"));
 	printf("%0.1f\n", d->age);
-	printf("%s\n", (d->owner ? d->owner :"nail"));
+	printf("%s\n", (d->owner ? d->owner :"nil"));
 
 
 }
