d9853a1234ec876d4a4280e5444a47e7ba78c8a6
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-21 13:08:57 -0800

    task added

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,14 +1,9 @@
 #include "dog.h"
 #include <stdio.h>
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		;
-
-	else
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
