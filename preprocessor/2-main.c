e6362003ccfdbe03394935e09589950f453fe06b
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-21 09:08:07 -0800

    task added

---
+++ preprocessor/2-main.c
@@ -0,0 +1,17 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
+#include <stdio.h>
+
+/**
+ *main - Program that prints the name of the file it was compiled from
+ *followed by a new line
+ *Return: 0 if correct
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
