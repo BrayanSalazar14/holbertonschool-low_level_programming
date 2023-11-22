89209bd99d6a406c269643f99a82e418da5c2a36
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-22 09:16:54 -0800

    hot fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,8 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t))
