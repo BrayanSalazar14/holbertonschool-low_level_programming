d3108cf44aa1f30ac2006de81b4912bd5af38981
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-22 09:09:46 -0800

    Hot fix

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDod = malloc(sizeof(dog_t))
+
+		if (new_dog == NULL)
+			return NULL;
+
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return NULL;
+	}
+
+	newDog->age = age;
+
+	if (newDog->owner == NULL)
+	{
+		free(newDog);
+		return NULL;
+	}
+
+	return newDog;
+}
+
