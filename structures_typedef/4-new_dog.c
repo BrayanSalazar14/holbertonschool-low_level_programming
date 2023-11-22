e1be6de553f77fc22a901838063b29b4f59f124d
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-22 13:30:24 -0800

    task added

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,20 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ *new_dog - Function that creates and initializes a new struct dog
+ *@name: Pointer to a string representing the dog's name
+ *@age: Float value representing the dog's age
+ *@owner: Pointer to a string representing the owner's name
+ *
+ *Return: Pointer to the newly created dog_t structure
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	size_t nameLength = strlen(name);
@@ -35,6 +47,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i <= ownerLength; i++)
 		newDog->owner[i] = owner[i];
 
-	return newDog;
+	return (newDog);
 }
 
