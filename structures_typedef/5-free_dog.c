0653f18e7fda21d6040a7e8c44412c9d2d70da01
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-22 15:39:20 -0800

    task add

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
+#include "dog.h"
+
+/**
+ * free_dog - Frees the memory allocated for a dog structure.
+ * @d: Pointer to a dog_t structure.
+ *
+ * This function frees the memory allocated for the dynamically
+ * allocated fields of the provided dog_t structure, including
+ * the name and owner fields. It also frees the memory for the
+ * structure itself.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	/* d->age no se libera por que es un tipo de dato primitivo "int"
+	   no es un puntero o una memoria asignada dinamicamente */
+	free(d->owner);
+	free(d);
+}
