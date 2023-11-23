6da1bf73a3226b5ee0baf36de360d22e8c369338
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-23 15:56:00 -0800

    task correction

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -12,11 +12,15 @@ int (*get_op_func(char *s))(int, int)
     };
     int i = 0;
 
-    while(*(ops[i].op) != *s)
-	    i++;
-
-    return(ops[i].f);
+    while(ops[i].op != NULL)
+    {
+	    if (*(ops[i].op == *s && s[i] == '\0'))
+	    {
+		    return (ops[i].f);
+	    }
+    }
 
     printf("Error\n");
     exit(99);
+
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -19,6 +19,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
+		printf("Error\n");
 		exit(100);
 	}
 	return (a / b);
@@ -28,6 +29,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
+		printf("Error\n");
 		exit(100);
 	}
 	return (a % b);
