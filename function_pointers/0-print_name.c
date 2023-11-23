74fb86af66305457c8c0896dd28e871189795001
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-23 07:56:05 -0800

    Hot fix

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,16 @@
 #include "function_pointers.h"
 
+/**
+ *print_name - Prints a name using a specified function.
+ *@name: Pointer to the name string.
+ *@f: Pointer to the printing function.
+ *
+ *Calls the specified printing function with the name as an argument.
+ *
+ *Return: void.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+		f(name);
 }
