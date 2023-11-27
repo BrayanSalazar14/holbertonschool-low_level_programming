690b36159a6d275702e447f742874a48b07fc63e
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-27 15:59:50 -0800

    Hot fix

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		char *string = va_arg(arg, char *);
+		if (string != NULL)
+			printf("%s", string);
+
+		else
+			printf("nil");
+
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+    }
+	va_end(arg);
+	printf("\n");
+}
