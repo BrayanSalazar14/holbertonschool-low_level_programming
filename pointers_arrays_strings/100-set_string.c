7c133f575a2c05f1fcad748bd643dfd212ed8115
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-11-28 11:18:59 -0800

    task add

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,20 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+#include "main.h"
+
+/**
+ *set_string - Function that sets the value of a string.
+ *@s: Pointer to a pointer to char where the value will be set.
+ *@to: Pointer to a constant char representing the new value.
+ *
+ *This function sets the value of the string pointed to by 's' to the
+ *value of the string pointed to by 'to'.
+ */
+void set_string(char **s, char *to)
+{
+	if (*s != NULL)
+		*s = to;
+	else
+		*s = to;
+}
