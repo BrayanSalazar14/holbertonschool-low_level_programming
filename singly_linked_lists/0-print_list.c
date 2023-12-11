5d175f4d558dfb65d0d97ce74980122e90d2a2cd
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-12-11 13:29:13 -0800

    Add new Project

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr;
+
+	ptr = h;
+
+	size_t count = 0;
+
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
+
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
