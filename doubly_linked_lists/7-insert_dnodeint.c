acdd2b1d08920b5813150b97db83c79b0e01f8e0
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-12-13 21:38:15 -0800

    change for code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -28,6 +28,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = NULL;
 	new_node->prev = NULL;
 
+	if (*h == NULL)
+	{
+		*h = new_node->next;
+		return (new_node);
+	}
+
 	if (idx == 0)
 	{
 		new_node->next = *h;
