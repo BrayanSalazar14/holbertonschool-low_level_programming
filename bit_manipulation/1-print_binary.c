dcb51e7383bab1368b87dd0409c4562762ca61f3
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-12-15 06:40:49 -0800

    Add task

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+/*
+ *Author: Brayan Steven Salazar
+ */
+
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of an unsigned long int.
+ * @n: The number to be converted and printed in binary.
+ *
+ * Description: Recursively converts the given unsigned long int to its
+ * binary representation and prints it to the standard output.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n / 2);
+
+	_putchar(n % 2 + '0');
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,5 +4,6 @@
 #include <stdlib.h>
 int _putchar(char ch);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
