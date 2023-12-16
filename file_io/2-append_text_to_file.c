9b02ff302764158e5fa21b39a865388812d2784b
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2023-12-15 21:00:14 -0800

    hot fix

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[count] != NULL)
+		while (text_content[count] != '\0')
 			count++;
 	}
 
