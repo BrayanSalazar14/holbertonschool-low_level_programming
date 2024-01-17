3f1c04e54a56a54dd73cbdc8ad887acbdcd1f098
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-01-17 08:24:58 -0800

    exercise change

--- practiceC/eliminarElementoVector.c
+++ practiceC/eliminarElementoVector.c
@@ -1,44 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void eliminar(int array[], int *cant, int position);
 void mostrarArray(int array[], int cant);
+void insertarVector(int array[], int *cantidad);
 
 int main()
 {
-	int cantidad, i = 0;
-	char decision;
-	printf("Ingrese la cantidad del arreglo:\n");
-	scanf("%d", &cantidad);
+	int opcion, cantidad = 0, array[100], posicion;
+	printf("-----BIENVENIDOS----\n");
 
-	int array[cantidad];
-
-	for (i = 0; i<cantidad; i++)
+	while(1)
 	{
-		printf("\nAgregue el valor del elemento[%d]:\n", i + 1);
-		scanf("%d", &array[i]);
+		printf("Ingrese una de las siguientes opciones:\n");
+		printf("[1]Ingresar vector\n[2]Mostrar vector\n[3]Eliminar elemento vector\n[4]Salir\n");
+		printf("\n");
+		scanf("%d", &opcion);
+
+		switch(opcion)
+		{
+			case 1:
+				printf("\nIngrese el numero de elementos del vector:\n");
+				scanf("%d", &cantidad);
+				printf("\n");
+				insertarVector(array, &cantidad);
+				break;
+			case 2:
+				mostrarArray(array, cantidad);
+				printf("\n");
+				break;
+			case 3:
+				printf("\nIngrese la posición a eliminar:\n");
+				scanf("%d", &posicion);
+				eliminar(array, &cantidad, posicion);
+				break;
+			case 4:
+				printf("Hasta la proxima\n");
+				exit(0);
+			default:
+				printf("Opcion no valida\n");
+				continue;
+		}
 	}
-	printf("\n");
-
-	printf("Digite Y si desea mostrar el vector, o N si desea eliminar un elemento del vector:\n");
-	scanf(" %c", &decision);
-
-	if (decision == 'Y')
-		mostrarArray(array, cantidad);
-	else if (decision == 'N')
-	{
-		int posicion;
-
-		printf("\nIngrese la posición a eliminar:\n");
-		scanf("%d", &posicion);
-
-		eliminar(array, &cantidad, posicion);
-
-		printf("El vector quedo de la siguiente forma:\n");
-		mostrarArray(array, cantidad);
-	}
-	else
-		printf("Hasta la proxima!");
-
 	return (0);
 }
 
@@ -58,4 +61,16 @@ void eliminar(int array[], int *cant, int position)
 		array[i] = array[i + 1];
 
 	*cant = *cant - 1;
+}
+
+void insertarVector(int array[], int *cantidad)
+{
+	int i = 0;
+
+	for (i = 0; i < *cantidad; i++)
+	{
+		printf("Digite el elemento [%d]:\n", i);
+		scanf("%d", &array[i]);
+	}
+	printf("\n");
 }
