6029c482d8738816961fe8239b6d36bd8499d4ef
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-01 18:54:33 -0800

    new task avanced

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function calculates the sum of all the multiples of 3 or 5 below 1024.
+ * It iterates through numbers from 0 to 1023 and adds those that are multiples
+ * of 3 or 5 to the sum.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, suma = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			suma += i;
+	}
+	printf("La suma es: [%d]\n", suma);
+}
