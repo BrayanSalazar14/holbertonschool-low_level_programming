b9bdd9bc775c5859dfc933833dbe092f3ef1d826
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-01 19:44:50 -0800

    new task avanced

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 
-int main() {
-    long int a = 1; 
-    long int b = 2; 
-    long int next;  
-    long int i;
+/**
+ * main - Entry point of the program
+ *
+ * This function calculates the sum of the even-valued terms in the Fibonacci
+ * sequence whose values do not exceed 4,000,000. It starts with the initial
+ * terms 1 and 2, and then calculates each subsequent term by adding the
+ * previous two terms. It stops when the next term exceeds 4,000,000. It then
+ * sums up all the even-valued terms and prints the result.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int a = 1;
+	long int b = 2;
+	long int next, sum = b;
 
-    
-    printf("%lu, %lu, ", a, b);
+	while (1)
+	{
+		next = a + b;
 
-  
-    for (i = 3; i <= 50; i++) {
-        next = a + b;
-        printf("%lu", next);
-        if (i < 50) {
-            printf(", ");
-        }
-        a = b;
-        b = next;
-      if (next > 4000000)
-      {
-        break;
-      }
-    }
-    printf("\n");
+		if (next > 4000000)
+			break;
 
-    return 0;
+		if (next % 2 == 0)
+			sum += next;
+		
+		a = b;
+		b = next;
+	}
+	printf("%lu ", sum);
+	printf("\n");
+	return (0);
 }
