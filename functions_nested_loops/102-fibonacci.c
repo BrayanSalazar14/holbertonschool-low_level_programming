698872d1819b00de58808acd11377904e5436113
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-01 19:28:16 -0800

    changes format datatype

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -3,18 +3,21 @@
 /**
  * main - Entry point of the program
  *
- * This function generates and prints the first 50 terms of the Fibonacci sequence.
- * It starts with the initial terms 1 and 2, and then calculates each subsequent term
- * by adding the previous two terms. The sequence is printed comma-separated until
+ * This function generates and prints
+ * the first 50 terms of the Fibonacci sequence.
+ * It starts with the initial terms 1 and 2, and then
+ * calculates each subsequent term
+ * by adding the previous two terms.
+ * The sequence is printed comma-separated until
  * the 50th term, followed by a newline.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	long a = 1; 
-	long b = 2; 
-	long next;  
+	long a = 1;
+	long b = 2;
+	long next;
 	long i;
 
 	printf("%lu, %lu, ", a, b);
