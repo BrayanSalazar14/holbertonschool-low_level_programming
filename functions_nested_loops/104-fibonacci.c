a843c8b884b1e0fe70eaf1581185d1d1aa72caea
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-01 20:50:50 -0800

    hot fix

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 
-int main() {
-  unsigned long int a = 1, b = 2, temp;
-  int count = 0;
-  while (count < 98) {
-	  printf("%lu, ", a);
-      temp = a;
-      a = b;
-      b = temp + b;
-      count++;
-  }
-    printf("%lu\n", a);
+/**
+ * main - Entry point of the program
+ *
+ * This function generates and prints the first 100 terms of the Fibonacci
+ * sequence. It starts with the initial terms 1 and 2, and then calculates
+ * each subsequent term by adding the previous two terms. The sequence is
+ * printed comma-separated until the 98th term, followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	long int a = 1, b = 2, next, i;
 
-    return 0;
+	printf("%lu, %lu, ", a, b);
+
+	for (i = 2; i < 98; i++)
+	{
+		next = a + b;
+		printf("%lu", next);
+
+		if (i < 97)
+			printf(", ");
+
+		a = b;
+		b = next;
+	}
+	printf("\n");
+	return (0);
 }
