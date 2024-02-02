662629b67765d7ca8439ab83062b3e396282af48
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-01 20:11:57 -0800

    hot fix

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -4,16 +4,16 @@ int main() {
     long int a = 1; 
     long int b = 2; 
     long int next;  
-    long int i, cant = 3;
+    long int i, cant = 2;
 
     
     printf("%lu, %lu, ", a, b);
 
   
-    for (i = 3; cant <= 98; i++) {
+    for (i = 3; cant <= 97; i++) {
         next = a + b;
         printf("%lu", next);
-        if (i < 98) {
+        if (i < 97) {
             printf(", ");
         }
         a = b;
