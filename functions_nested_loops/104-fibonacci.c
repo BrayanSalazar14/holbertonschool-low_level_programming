5acc95d424e8f56deefbe5346e1715c731a3db25
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-02-01 20:44:31 -0800

    hot fix

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -4,17 +4,13 @@ int main() {
   unsigned long int a = 1, b = 2, temp;
   int count = 0;
   while (count < 98) {
-      if (count == 97) {
-          printf("%lu\n", a);
-      } else {
-          printf("%lu, ", a);
-      }
+	  printf("%lu, ", a);
       temp = a;
       a = b;
       b = temp + b;
       count++;
   }
-    printf("\n");
+    printf("%lu\n", a);
 
     return 0;
 }
